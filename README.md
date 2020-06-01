bda6ea48a6dd362088bce57e982a60699a86f492
Jose Juan Jaramillo Polo <josejuan.jaramillopolo@gmail.com>
2020-05-31 22:47:37 -0500

    V1.0.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# Linear Equation Solver 3x3
+
+Console based application that solves 3x3 linear equation systems for you. It uses Cramer's and Sarrus's methods and gives you the result for each determinant and the solutions for the 3 unknown variables in the system.
+
+## Release notes: 
+* Gives you the value for each determinant
+* Gives you the value for each solution (x y z)
+
+## Compilation requirements
+* It should compile with almost every C++ compiler
+* I am using Code::Blocks and GCC currently.
---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+void inputCoeficients(int *aux, const char* vectorName)
+{
+    std::cout << "---------------------------> " << vectorName << std::endl;
+    for(int i = 0 ; i< 3 ; i++)
+    {
+        std::cout << "Input coefficient " << vectorName << "[" << i+1 << "] : ";
+        std::cin >> *aux;
+        aux ++;
+    }
+    std::cout << std::endl;
+}
+
+displayEqSys(int *x, int *y, int *z, int *r)
+{
+    for (int i = 0 ; i < 3 ; i++)
+    {
+        std::cout << "(" << *x << ")x + " << "(" << *y << ")y + " << "(" << *z << ")z + " << " = " << *r << std::endl;
+        x++; y++; z++; r++;
+    }
+}
+
+int solveMatrix(int x[3], int y[3], int z[3])
+{
+    return ((x[0] * y[1] * z[2]) + (x[1] * y[2] * z[0]) + (x[2] * y[0] * z[1])) - ((x[1] * y[0] * z[2]) + (x[0] * y[2] * z[1]) + (x[2] * y[1] * z[0])) ;
+}
+
+
+int main()
+{
+    int x[3];
+    int y[3];
+    int z[3];
+    int r[3];
+
+    //data input
+    std::cout << "=== Equation Solving system Using Cramer's And Sarrus's methods === " << std::endl << std::endl;
+    inputCoeficients(&x[0], "x");
+    inputCoeficients(&y[0], "y");
+    inputCoeficients(&z[0], "z");
+    inputCoeficients(&r[0], "independent term");
+
+    // display entry equations system
+    std::cout << std::endl << "=== Equation System === " << std::endl;
+    displayEqSys(&x[0],&y[0],&z[0],&r[0]);
+
+    //Solving assembling
+    int deltaS = solveMatrix(x,y,z);
+    int deltaX = solveMatrix(r,y,z);
+    int deltaY = solveMatrix(x,r,z);
+    int deltaZ = solveMatrix(x,y,r);
+
+    std::cout << std::endl << "=== Determinants === " << std::endl;
+    std::cout << "Delta s = " << deltaS << std::endl;
+    std::cout << "Delta x = " << deltaX << std::endl;
+    std::cout << "Delta y = " << deltaY << std::endl;
+    std::cout << "Delta z = " << deltaZ << std::endl;
+
+    //Getting Solutions
+    int solveX = deltaX / deltaS;
+    int solveY = deltaY / deltaS;
+    int solveZ = deltaZ / deltaS;
+
+    std::cout << std::endl << "=== Solutions === " << std::endl;
+    std::cout << "x = " << solveX << std::endl;
+    std::cout << "y = " << solveY << std::endl;
+    std::cout << "z = " << solveZ << std::endl;
+
+    return 0;
+}
